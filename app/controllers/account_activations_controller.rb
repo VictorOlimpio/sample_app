3741ebacf8e8f28141215539a3db1494986898ea
Victor Olimpio <victorwebwork@gmail.com>
2017-09-20 12:52:30 -0300

    Adequa estilo de código

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,16 +1,19 @@
 class AccountActivationsController < ApplicationController
-
   def edit
     user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-     user.activate
+    if user_check user
+      user.activate
       log_in user
-      flash[:success] = "Account activated!"
+      flash[:success] = 'Account activated!'
       redirect_to user
     else
-      flash[:danger] = "Invalid activation link"
+      flash[:danger] = 'Invalid activation link'
       redirect_to root_url
     end
   end
 
+  def user_check(user)
+    return true if !user&.activated? &&
+        user&.authenticated?(:activation, params[:id])
+  end
 end
